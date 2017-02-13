ad6f7744233dc4176febcd9be6038f16c42feec4
Gabriel Esteban <gabriel.esteban.gullon@gmail.com>
2017-02-13 17:32:22 +0100

    Added skel of react components

---
+++ src/components/ContactList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './ContactList.css';
+
+class ContactList extends Component {
+  render() {
+    return (
+      <div className="ContactList">
+
+      </div>
+    );
+  }
+}
+
+export default ChatApp;
---
+++ src/components/Message.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './MessageList.css';
+
+class MessageList extends Component {
+  render() {
+    return (
+      <div className="MessageList">
+
+      </div>
+    );
+  }
+}
+
+export default ChatApp;
